b28c476b8d3d1c53a1867b89b10c487bce03f9d0
ramalah amir <ramalahamir@gmail.com>
2024-10-27 19:38:45 -0700

    setting up basic structure of the code

--- DS_C_A3_i232644.cpp
+++ DS_C_A3_i232644.cpp
@@ -1,4 +1,45 @@
 #include <iostream>
 using namespace std;
 
+struct GamesPlayedNode
+{
+    int gameID;
+    float hoursPlayed;
+    int achievements;
+};
+
+class GamesPlayedTree
+{
+};
+
+struct PlayerNode
+{
+    int playerID;
+    string name;
+    string phone_no;
+    string email;
+    string password;
+
+    // each node will have its own games played tree structure
+    GamesPlayedTree gamesPlayed;
+};
+
+class PlayerTree
+{
+};
+
+struct GameNode
+{
+    int gameID;
+    string name;
+    string developer;
+    string publisher;
+    float fileSize;
+    int downloads;
+};
+
+class GameTree
+{
+};
+
 int main() { return 0; }
